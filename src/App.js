c8b1ec33ec97f44ed79e1713dfe7c8556b3dcc78
Abul Kalam Azad <dev.akazad@hotmail.com>
2022-02-27 15:58:21 +0600

    navbar, header and other components done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Blog, Footer, Header, Possibility, WhatGPT3 } from './containers';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Article />
+        <Feature />
+        <Possibility />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./cta.css";
+
+const CTA = () => {
+  return <div>CTA section</div>;
+};
+
+export default CTA;
